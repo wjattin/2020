15382af8af3593e1b682cb8f3d0d02a14ce36f68
William Jattin <wjattin@miami.edu>
2020-01-21 15:16:50 -0500

    Week 2 move code

---
+++ cimx16/scripts/week_2/move.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    public float speed  = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow)){
+            //do somehing
+            transform.position = new Vector3(transform.position.x - speed * Time.deltaTime, transform.position.y, transform.position.z);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            //do somehing
+            transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y, transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            //do somehing
+            transform.position = new Vector3(transform.position.x , transform.position.y, transform.position.z + speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            //do somehing
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - speed * Time.deltaTime);
+        }
+    }
+}
